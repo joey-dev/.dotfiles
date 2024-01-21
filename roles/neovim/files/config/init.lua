0c34415fb8a5ad36f4e5dd730fd6f1fc7dedb39d
joey <joeystil3@gmail.com>
2024-01-21 11:12:39 +0100

    Added nvim - projects + tree + git

--- roles/neovim/files/config/init.lua
+++ roles/neovim/files/config/init.lua
@@ -2,17 +2,15 @@ local vim = vim
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-require("plugins")
+vim.g.mapleader = " "
 
 vim.cmd("language en_US.utf8")
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true })
-vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap("n", "<leader>vi", ":e ~/.dotfiles/roles/neovim/files/conf/init.lua<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>vc", "<cmd>lua EditFtPluginFile()<cr>", { noremap = true })
-function _G.EditFtPluginFile()
-  vim.cmd("e ~/.dotfiles/roles/neovim/files/ftplugin/" .. vim.bo.filetype .. ".vim")
-end
+require("plugins")
+
+vim.api.nvim_set_keymap("n", "<leader>vi", ":e ~/.dotfiles/roles/neovim/files/config/init.lua<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>vs", ":source ~/.dotfiles/roles/neovim/files/config/init.lua<cr>", { noremap = true })
 
 -- nvim settings
 vim.o.completeopt = "menuone,noinsert,noselect"
@@ -28,5 +26,24 @@ vim.o.relativenumber = true
 vim.o.undolevels = 1000
 vim.o.so = 100
 
--- require plugins
 
+-- global keymap
+-- projects
+vim.keymap.set('n', '<Leader>pl', ':ProjectList<cr>')
+vim.keymap.set('n', '<Leader>fif', ':ProjectFindInFiles<cr>')
+vim.keymap.set('n', '<Leader>ff', ':ProjectSearchFiles<cr>')
+
+-- task manager
+vim.keymap.set('n', '<Leader>tm', ':ProjectRun<cr>')
+
+-- tree
+vim.keymap.set('n', '<Leader>pf', ':Neotree<cr>')
+vim.keymap.set('n', '<Leader>pcf', ':Neotree reveal<cr>')
+vim.keymap.set('n', '<Leader>pof', ':Neotree buffers<cr>')
+
+--- git
+vim.keymap.set('n', '<Leader>gs', ':Neotree git_status<cr>')
+
+-- view
+vim.keymap.set('n', '<Leader>vm', ':MarkdownPreview<cr>')
+vim.keymap.set('n', '<Leader>vM', ':MarkdownPreviewStop<cr>')
