2988ad563d66ef91b0029987db520d7ef602e2b9
joey <joeystil3@gmail.com>
2024-01-22 08:21:17 +0100

    nvim - added support for phpactor, added 2 test commands

--- roles/neovim/files/config/lua/plugins.lua
+++ roles/neovim/files/config/lua/plugins.lua
@@ -44,5 +44,27 @@ require("lazy").setup({
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
 		build = function() vim.fn["mkdp#util#install"]() end,
+	},
+	{
+	  "VonHeikemen/lsp-zero.nvim",
+	  dependencies = {
+		-- LSP Support
+		{"neovim/nvim-lspconfig"},
+		{"williamboman/mason.nvim"},
+		{"williamboman/mason-lspconfig.nvim"},
+
+		-- Autocompletion
+		{"hrsh7th/nvim-cmp", event = "InsertEnter"},
+		{"hrsh7th/cmp-buffer"},
+		{"hrsh7th/cmp-path"},
+		{"saadparwaiz1/cmp_luasnip"},
+		{"hrsh7th/cmp-nvim-lsp"},
+		{"hrsh7th/cmp-nvim-lua"},
+
+		-- Snippets
+		{"L3MON4D3/LuaSnip", event = "InsertEnter"},
+		{"rafamadriz/friendly-snippets"},
+	  },
 	}
+
 })
