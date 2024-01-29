2a5d12a9de8b6a2a3bc983485d328ee2ab43366a
joey <joeystil3@gmail.com>
2024-01-29 08:08:51 +0100

    nvim - added custom snippets

--- roles/neovim/files/config/init.lua
+++ roles/neovim/files/config/init.lua
@@ -242,3 +242,43 @@ dap.configurations.php = {
   }
 }
 
+
+-- snippets
+
+local ls = require("luasnip")
+-- some shorthands...
+local snip = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamicn = ls.dynamic_node
+
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+
+
+local date = function() return {os.date('%Y-%m-%d')} end
+
+ls.add_snippets(nil, {
+    php = {
+		snip({
+			trig = "pubf",
+			dscr = "Public function",
+			priority=1000,
+			regTrig=false,
+		},
+		{
+			text({"public function ",}), insert(1, "name"), text({"(",}), insert(2, "params"), text({"): ",}), insert(3, "return type"), text({"",
+			"{"}), text({"",
+			""}), insert(0), text({"",
+			""}), text({"}"})
+		}
+	  ),
+    },
+})
