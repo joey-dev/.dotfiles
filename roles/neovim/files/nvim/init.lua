2d0308587a5f66aab6dedde21d8422467ac75a57
joey <joeystil3@gmail.com>
2024-04-27 10:18:39 +0200

    fix formatting

--- roles/neovim/files/nvim/init.lua
+++ roles/neovim/files/nvim/init.lua
@@ -245,17 +245,17 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
 		['<CR>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-            if luasnip.expandable() then
-                luasnip.expand()
-            else
-                cmp.confirm({
-                    select = true,
-                })
-            end
+			if cmp.visible() then
+        if luasnip.expandable() then
+          luasnip.expand()
         else
-            fallback()
+          cmp.confirm({
+						select = true,
+					})
         end
+      else
+				fallback()
+      end
     end),
 
     ["<Tab>"] = cmp.mapping(function(fallback)
