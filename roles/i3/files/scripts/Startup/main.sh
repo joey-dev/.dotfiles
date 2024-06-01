639fb6b93c4616dce684fa87090e284da4153c5e
Joey <joeystil3@gmail.com>
2024-06-01 21:43:49 +0200

    [Startup] Ask to update npm, php, and ask to update system (#34)

--- roles/i3/files/scripts/Startup/main.sh
+++ roles/i3/files/scripts/Startup/main.sh
@@ -9,6 +9,10 @@ while read i; do
     names+=($name)
 done < <(jq -c '.[]' $CONFIG_FILE)
 
+if ! zsh -c "ssh-add -l" &>/dev/null; then
+	zsh -c "ssh-add"
+fi
+
 printf -v namesString '%s,' "${names[@]}"
 echo "What profile do you want?"
 echo $namesString
@@ -29,10 +33,23 @@ if [ ! -v task ]; then
 fi
 
 zsh -c "task context $task"
-zsh -c "~/.dotfiles/roles/i3/files/scripts/Windows/Update/npm $nodeVersion"
-zsh -c "~/.dotfiles/roles/i3/files/scripts/Windows/Update/php $phpVersion"
 
-if ! zsh -c "ssh-add -l" &>/dev/null; then
-	zsh -c "ssh-add"
+echo "Update NPM and PHP version to current profile?"
+echo "y/n"
+read updateVersion
+
+if [ "$updateVersion" = "y" ]; then
+	zsh -c "~/.dotfiles/roles/i3/files/scripts/Windows/Update/npm $nodeVersion"
+	zsh -c "~/.dotfiles/roles/i3/files/scripts/Windows/Update/php $phpVersion"
+fi
+
+echo "Update System?"
+echo "y/n"
+read updateSystem
+
+if [ "$updateSystem" = "y" ]; then
+	zsh -c "cd ~/.dotfiles && git checkout master"
+	zsh -c "cd ~/.dotfiles && git pull"
+	zsh -c "cd ~/.dotfiles && make provision"
 fi
 
