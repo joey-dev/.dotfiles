1c2f29cde67d06e7fb8ab34b1ac229db9c65590e
Joey <joeystil3@gmail.com>
2024-06-14 14:07:51 +0200

    [Nvim] Add auto save (#42)

--- roles/neovim/files/nvim/lua/plugins/common.lua
+++ roles/neovim/files/nvim/lua/plugins/common.lua
@@ -78,6 +78,41 @@ return {
 		end,
 	},
 	{
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup {
+				enabled = true,
+				execution_message = {
+					message = function()
+						return ("AutoSaved: " .. vim.fn.strftime("%H:%M:%S"))
+					end,
+					dim = 0.18,
+					cleaning_interval = 2500,
+				},
+				trigger_events = {"InsertLeave", "TextChanged"},
+				condition = function(buf)
+					local fn = vim.fn
+					local utils = require("auto-save.utils.data")
+					if
+						fn.getbufvar(buf, "&modifiable") == 1 and
+						utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+						return true
+					end
+					return false
+				end,
+				write_all_buffers = false,
+				debounce_delay = 135,
+				callbacks = {
+					enabling = nil,
+					disabling = nil,
+					before_asserting_save = nil,
+					before_saving = nil,
+					after_saving = nil,
+				}
+			}
+		end
+	},
+	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
