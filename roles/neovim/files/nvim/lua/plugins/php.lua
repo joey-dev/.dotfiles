673b3ec0f32848e2bdfa6b26e41c42d72f915e8f
Joey <joeystil3@gmail.com>
2024-08-28 14:58:05 +0200

    [Nvim][PHP] Fix phpactor and phpstan with prophecy (#52)
    * [Nvim][Php] Fix phpactor and phpstan with prophecy
    
    * Fix indenting
    
    * Add phpstan config to template
    
    * remove comment

--- roles/neovim/files/nvim/lua/plugins/php.lua
+++ roles/neovim/files/nvim/lua/plugins/php.lua
@@ -1,25 +1,14 @@
 return {
 	{
-		'gbprod/phpactor.nvim',
-		build = function() require('phpactor.handler.update') end, -- To install/update phpactor when installing this plugin
-		opts = {
-		  install = {
-			bin = vim.fn.stdpath('data') .. '/mason/bin/phpactor',
-		  },
-		  lspconfig = {
-			enabled = false,
-		  },
-		},
-		config = function(_, opts)
-		  local phpactor = require('phpactor')
-		  phpactor.setup(opts)
-
-		  vim.api.nvim_set_keymap('n', '<leader>pa', ':lua require("phpactor").rpc()<CR>', { noremap = true, silent = true })
+		"gbprod/phpactor.nvim",
+		build = function()
+			require("phpactor.handler.update")()
 		end,
-		ft = "php",
 		dependencies = {
-		  'nvim-lua/plenary.nvim',
-		  'neovim/nvim-lspconfig',
+			"nvim-lua/plenary.nvim",
+			"neovim/nvim-lspconfig"
+		},
+		opts = {
 		},
 	},
 	{
