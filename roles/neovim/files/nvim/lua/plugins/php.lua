4f4ec36c4534627aa3b16bab1b66f100560eccd8
joey-dev <test@gmail.com>
2024-08-28 14:35:26 +0200

    [Nvim][Php] Fix phpactor and phpstan with prophecy

--- roles/neovim/files/nvim/lua/plugins/php.lua
+++ roles/neovim/files/nvim/lua/plugins/php.lua
@@ -1,27 +1,17 @@
 return {
-	{
-		'gbprod/phpactor.nvim',
-		build = function() require('phpactor.handler.update') end, -- To install/update phpactor when installing this plugin
-		opts = {
-		  install = {
-			bin = vim.fn.stdpath('data') .. '/mason/bin/phpactor',
-		  },
-		  lspconfig = {
-			enabled = false,
-		  },
-		},
-		config = function(_, opts)
-		  local phpactor = require('phpactor')
-		  phpactor.setup(opts)
-
-		  vim.api.nvim_set_keymap('n', '<leader>pa', ':lua require("phpactor").rpc()<CR>', { noremap = true, silent = true })
-		end,
-		ft = "php",
-		dependencies = {
-		  'nvim-lua/plenary.nvim',
-		  'neovim/nvim-lspconfig',
-		},
-	},
+  {
+    "gbprod/phpactor.nvim",
+    build = function()
+      require("phpactor.handler.update")()
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig"
+    },
+    opts = {
+      -- you're options coes here
+    },
+  },
 	{
 		"alvan/vim-php-manual",
 	}
