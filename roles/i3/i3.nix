c5b8187651fa6f015371ea1bd4b67133d9ee84cd
Joey <joeystil3@gmail.com>
2026-02-25 13:08:45 +0000

    Fix i3 symlink config

--- roles/i3/i3.nix
+++ roles/i3/i3.nix
@@ -1,14 +1,11 @@
 { config, pkgs, lib, ... }:
 
-# Wrap the entire configuration in mkIf so none of this evaluates on macOS/Darwin
 lib.mkIf pkgs.stdenv.isLinux {
 
-  # 1. Use native Home Manager modules for your core services
   xsession.windowManager.i3.enable = true;
   services.picom.enable = true;
   services.dunst.enable = true;
 
-  # 2. Install supplementary packages
   home.packages = with pkgs; [
     i3status
     i3lock
@@ -21,15 +18,11 @@ lib.mkIf pkgs.stdenv.isLinux {
     python3
   ];
 
-  # 3. Consolidate file linking using xdg.configFile consistently
   xdg.configFile = {
-    # Link the main config
-    "i3/config".source = ./configuration/i3/config;
+    "i3/config".source = lib.mkForce ./configuration/i3/config;
     
-    # Link the entire scripts directory cleanly
     "i3/scripts".source = ./configuration/scripts;
 
-    # Link specific executable scripts
     "i3/i3-move-display.sh" = {
       source = ./configuration/i3/i3-move-display.sh;
       executable = true;
