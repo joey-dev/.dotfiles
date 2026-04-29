d87e6ec16e61ee2d54c3e30e5252105055f0e1b1
joey stil <joey.stil@dyflexis.com>
2026-04-29 15:26:34 +0200

    [Nvim] Add syntax highlighting to sql

--- roles/neovim/configuration/nvim/lua/plugins/treesitter.lua
+++ roles/neovim/configuration/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-      ensure_installed = { "vue", "css", "scss" }, -- This is correct
+      ensure_installed = { "vue", "css", "scss", "sql" },
     },
   },
 }
